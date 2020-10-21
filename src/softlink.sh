f494cb89ed58496ab81087d3900016303cb13179
dstrassera <strassera.davide@gmail>
2020-10-21 12:51:15 +0100

    added Makefile and scripts for tests and hooks

---
+++ src/softlink.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# create a soft link for pre-commit in .git/hooks/
+
+FILE=../.git/hooks/pre-commit.sample
+if [[ -f $FILE ]]; then
+	rm "$FILE" 
+	echo "remouved $FILE"
+fi
+
+FILE2=../.git/hooks/pre-commit
+if [[ -f $FILE2 ]]; then
+	rm "$FILE2" 
+	echo "removed $FILE2"
+fi
+
+BASEPATH=$(pwd)
+ln -s $BASEPATH/pre-commit ../.git/hooks/pre-commit
+
+if [[ $? -eq 0 ]]; then
+	echo "created .git/hooks/pre-commit symbolic link"
+fi
---
+++ src/tests.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# command to run
+python3 test_SetupFileLoader.py
+t1=$?
+python3 test_AccEnvCalc.py
+t2=$?
+python3 test_LapTimeSimCalc.py
+t3=$?
+python3 test_RunOpenLapSim.py
+t4=$?
+
+# based on the output code $? (0 is OK else error)
+if [ $t1 -ne 0 ] || [ $t2 -ne 0 ] || [ $t3 -ne 0 ] || [ $t4 -ne 0 ]; then
+	cat < "This commit is aborted because test has failed"
+	exit 1
+else
+	echo "pre-commit (git hooks): All Testes Passed"
+fi
